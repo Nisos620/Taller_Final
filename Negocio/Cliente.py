7f34769d26c38c5bb56092151b7eff0f53e99ca7
Nicolas Santander <josess.nicolas@hotmail.com>
2021-04-14 01:41:10 -0500

    Client basic functions

--- Negocio/Cliente.py
+++ Negocio/Cliente.py
@@ -1,8 +1,13 @@
+from Negocio.Cuenta import Cuenta
+
+
 class Cliente:
+
     def __init__(self,nombre,usuario,identificacion):
         self.nombre= nombre
         self.usuario= usuario
         self.identificacion= identificacion
+        self.lista_cuentas = list()
 
     def getNombre(self):
         return self.nombre
@@ -13,6 +18,9 @@ class Cliente:
     def getIdentificacion(self):
         return self.identificacion
 
+    def get_lista_cuentas(self):
+        return self.lista_cuentas
+
     def setnombre(self,nombre):
         self.nombre=nombre
 
@@ -20,4 +28,18 @@ class Cliente:
         self.usuario=usuario
 
     def setIdentificacion(self,identificacion):
-        self.identificacion=iden tificacionpro
+        self.identificacion=identificacion
+
+    def añadir_cuenta(self, id, monto_inicial, minimo, porcentaje, saldo):
+        existe_cuenta = -1
+        index = 0
+        for i in self.lista_cuentas:
+            if i.get_id() == id:
+                existe_cuenta = index
+                index += 1
+
+        if existe_cuenta > -1:
+            print('El id ya está registrado')
+        else:
+            self.lista_cuentas.append(Cuenta(id, monto_inicial, minimo, porcentaje, saldo))
+            print('Se agregó la cuenta')
--- Negocio/Cuenta.py
+++ Negocio/Cuenta.py
@@ -1,9 +1,13 @@
 class Cuenta:
-    def __init__(self,monto_inicial,minimo,porcentaje,saldo):
+    def __init__(self,id, monto_inicial,minimo,porcentaje,saldo):
+        self.id = id
         self.monto_inicial= monto_inicial
         self.minimo=minimo
         self.porcentaje=porcentaje
         self.saldo=saldo
+
+    def get_id(self):
+        return self.id
 
     def getMontoInicial(self):
         return self.monto_inicial
@@ -16,6 +20,9 @@ class Cuenta:
 
     def getSaldo(self):
         return self.saldo
+
+    def set_id(self, id):
+        self.id = id
 
     def setMontoInicial(self,monto):
         self.monto_inicial=monto
